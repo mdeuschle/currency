45064b5ad1dc061d6901d4a5452b13e296c8032c
Matt Deuschle <mdeuschle@gmail.com>
2017-09-19 14:59:40 -0700

    final UI and util

---
+++ Currency-converter/Currency-converter/CalculatorVC.swift
@@ -0,0 +1,23 @@
+//
+//  CalculatorVC.swift
+//  Currency-converter
+//
+//  Created by Matt Deuschle on 9/19/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+
+    @IBOutlet var baseCurrencyAmountLabel: UITextField!
+    @IBOutlet var selectedCurrencyAmountLabel: UITextField!
+    @IBOutlet var selectedCountryLabel: UILabel!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Currency-converter/Currency-converter/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Currency-converter
+//
+//  Created by Matt Deuschle on 9/19/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import Foundation
+
+enum ReusableCell: String {
+    case flag = "flagCell"
+}
+
+enum SegueString: String {
+    case goToCalculator = "goToCalculator"
+}
+
+enum URLString: String {
+    case calculationData = "http://fixer.io/"
+}
