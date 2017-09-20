d48790dfe476b3973e4bfc391f9ba7561444e4ac
Matt Deuschle <mdeuschle@gmail.com>
2017-09-19 17:11:47 -0700

    done button to keyboard

--- Currency-converter/Currency-converter/CalculatorVC.swift
+++ Currency-converter/Currency-converter/CalculatorVC.swift
@@ -13,11 +13,27 @@ class CalculatorVC: UIViewController {
     @IBOutlet var baseCurrencyAmountLabel: UITextField!
     @IBOutlet var selectedCurrencyAmountLabel: UITextField!
     @IBOutlet var selectedCountryLabel: UILabel!
-
+    @IBOutlet var baseTextField: UITextField!
+    @IBOutlet var selectedCurrencyTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addDoneButton()
+        baseTextField.font = .systemFont(ofSize: 32)
+        selectedCurrencyTextField.font = .systemFont(ofSize: 32)
+    }
 
+    func addDoneButton() {
+        let toolbarDone = UIToolbar()
+        toolbarDone.sizeToFit()
+        let doneBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(CalculatorVC.dismissVC))
+        toolbarDone.items = [doneBarButtonItem]
+        baseTextField.inputAccessoryView = toolbarDone        
+    }
+
+    func dismissVC() {
+        dismiss(animated: true, completion: nil)
     }
 
 }
+
